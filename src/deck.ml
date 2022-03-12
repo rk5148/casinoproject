27697c7ca217c859bc9b838b192ba3327148c04b
Eric Guo <eg455@cornell.edu>
2022-03-12 16:32:32 -0500

    Minor changes to type suit and make_deck

--- src/deck.ml
+++ src/deck.ml
@@ -1,9 +1,9 @@
 exception Empty
 
 type suit =
-  | Spade
-  | Diamond
-  | Heart
+  | Spades
+  | Diamonds
+  | Hearts
   | Clubs
 
 type card = {
@@ -20,6 +20,6 @@ let rec make_suit suit value =
       if x <= 13 then make_card suit value :: make_suit suit (value + 1)
       else raise Empty
 
-let rec make_deck =
-  make_suit Spade 0 @ make_suit Diamond 0 @ make_suit Heart 0
-  @ make_suit Clubs 0
+let make_deck =
+  make_suit Spades 2 @ make_suit Diamonds 2 @ make_suit Hearts 2
+  @ make_suit Clubs 2

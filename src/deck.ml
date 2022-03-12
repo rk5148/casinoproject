f04ca185da73d9394b47b3c6b8e7fae9950e8321
Aaron Yagnik <ay294@cornell.edu>
2022-03-12 16:13:19 -0500

    Some edits to deck.ml

--- src/deck.ml
+++ src/deck.ml
@@ -1,1 +1,15 @@
-type face_card = Color of int
+type suit =
+  | Spade
+  | Diamond
+  | Heart
+  | Clubs
+
+type card = {
+  suit : suit;
+  value : int;
+}
+
+let a_heart = { suit = Heart; value = 14 }
+
+let rec make_hearts last_val =
+  if last_val + 1 = 14 then { suit = Heart; value = 14 }

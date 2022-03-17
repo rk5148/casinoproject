ed817f3004b3f2d95bc3196733f188709fadc5ec
Alexander Lawless <ajl429@cornell.edu>
2022-03-16 20:44:33 -0400

    Added more acceptable user inputs in casino.ml

--- src/casino.ml
+++ src/casino.ml
@@ -4,19 +4,55 @@ open Wheeloffortune
 open Slots
 open Deck
 
+let wof_commands =
+  [
+    "1";
+    "1 ";
+    "Wheel of Fortune";
+    "wheel of fortune";
+    "WHEEL OF FORTUNE";
+    "Wheel of Fortune ";
+    "wheel of fortune ";
+    "WHEEL OF FORTUNE ";
+    "wof";
+    "WOF";
+    "wof ";
+    "WOF ";
+  ]
+
+let slots_commands =
+  [ "2"; "2 "; "Slots"; "slots"; "SLOTS"; "Slots "; "slots "; "SLOTS " ]
+
+let deal7_commands =
+  [
+    "3";
+    "3 ";
+    "Deal 7";
+    "deal 7";
+    "DEAL 7";
+    "Deal 7 ";
+    "deal 7 ";
+    "DEAL 7 ";
+    "Deal7";
+    "deal7";
+    "DEAL7";
+  ]
+
 (*Runs the game*)
 let rec main () =
   print_endline
     "\n\n\
      Welcome to The Casino\n\
-    \ What game do you want to play? (Wheel of Fortune), (Slots), \
-     (Deal 7).\n";
+    \ What game do you want to play? \n\
+    \ (1): Wheel of Fortune, \n\
+    \ (2): Slots, \n\
+    \ (3): Deal 7\n";
   print_string "> ";
   let command = read_line () in
-  if command = "Wheel of Fortune" then
+  if List.mem command wof_commands then
     Wheeloffortune.wheel_of_fortune ()
-  else if command = "Slots" then Slots.slots ()
-  else if command = "Deal 7" then Deck.play_deck ()
-  else print_endline "\nInvalid input, stupid."
+  else if List.mem command slots_commands then Slots.slots ()
+  else if List.mem command deal7_commands then Deck.play_deck ()
+  else print_endline "\nInvalid input."
 
 let _ = main ()
